b2ed957c5cabfc69bff82752dc81ac0355cd72fd
MHany <hany.f1387@gmail.com>
2022-08-02 18:57:35 +0430

    first commit

---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+from django.contrib.auth import get_user_model
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        # fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        # fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email',)
